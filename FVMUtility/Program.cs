59e60f3106fe7c14cee9a0d6a09fe40c05f61ae3
KRISH_ArjunA <arjun.aroskar@krishmark.com>
2026-08-20 11:09:53 +0530

    push fixation for fvu

--- FVMUtility/Program.cs
+++ FVMUtility/Program.cs
@@ -22,11 +22,11 @@ namespace FVUFileMove
             Console.WriteLine("       FVU File Move Started");
             Console.WriteLine("=================================");
 
-            //string mwBatchId =
-            //    args.Length > 0
-            //        ? args[0]
-            //        : string.Empty;
-            string mwBatchId = "30288";
+            string mwBatchId =
+                args.Length > 0
+                    ? args[0]
+                    : string.Empty;
+            // string mwBatchId = "30758";
 
 
             if (string.IsNullOrWhiteSpace(mwBatchId))
